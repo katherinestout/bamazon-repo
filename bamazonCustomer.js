5f43b0476ff2ccab96940ed01f08ce84218e3023
katie stout <katiestout@katies-MBP.home>
2018-06-28 08:52:42 -0400

    some changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,29 +22,60 @@ connection.connect(function(err){
 });
 //selecting all of the products
 function start(){
-    connection.query("SELECT * FROM products", function(err, results){
+    //connection.query("SELECT * FROM products", function(err, results){
     inquirer
     .prompt(
         {
             
-            name: "productId",
-            type: "input",
+            //name: "productId",
+            //type: "input",
             //choices: ["View Products for Sale!"],
-            message:"Enter the product you want to buy!",
+            //message:"Enter the product you want to buy!",
             //checkProduct();
-            },
-            {
-                name: "units",
-                type: "input",
-                message: "How many would you like to buy?"
-            }
-    )
-  
-    
-    ;
+            name: "choice",
+            type: "rawlist",
+            message: "View all of our [PRODUCTS] for sale, or [LIST] an item",
+            choices: ["PRODUCTS", "LIST"]
+            //},
+            //{
+             //   name: "units",
+              //  type: "input",
+              //  message: "How many would you like to buy?"
+            })
+            .then(function(answer){
+                if (answer.start.toUpperCase() === "PRODUCTS"){
+                    products();
+                }
+                else {
+                    list();
+                }
+            });
+        }
+
+function products(){
+    connection.query("SELECT * FROM products", function(err, results){
+    if (err) throw err;
+
+    inquirer
+    .prompt([
+        {
+    name: "productsUnits",
+    type: "rawlist",
+    choices: function() {
+    var choiceArray = [];
+    for (var i = 0; i < results.length; i++) {
+    choiceArray.push(results[i].product_name);
+    }
+     return choiceArray;
+     },
+    message: "Which Product?"
+    },
+    {
+    name: "units",
+    type: "input",
+    message: "How many units would you like?"
+    }
+    ])
+   // .then(function(answer){}
 
 
-        
-
-
-        }); }
